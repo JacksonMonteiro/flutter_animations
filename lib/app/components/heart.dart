b3903207293a4e538ee88d6ec2e1b60107581728
jacksonmonteiro <infor.jackson324@gmail.com>
2023-02-03 12:16:40 -0300

    animation controller

--- lib/app/components/heart.dart
+++ lib/app/components/heart.dart
@@ -5,16 +5,65 @@ class Heart extends StatefulWidget {
   _HeartState createState() => _HeartState();
 }
 
-class _HeartState extends State<Heart> {
+class _HeartState extends State<Heart> with SingleTickerProviderStateMixin {
+  bool isFavorite = false;
+  
+  late AnimationController _animationController;
+  late Animation _colorAnimation;
+  
+  @override
+  void initState() {
+    super.initState();
+    _animationController = AnimationController(
+      vsync: this,
+      duration: Duration(milliseconds: 500),
+    );
+
+    _colorAnimation = ColorTween(begin: Colors.grey[400], end: Colors.red)
+        .animate(_animationController);
+
+
+    _animationController.addListener(() {
+      
+    });
+
+    _animationController.addStatusListener((status) {
+      if (status == AnimationStatus.completed) {
+        setState(() {
+          isFavorite = true;
+        });
+      } 
+
+      if (status == AnimationStatus.dismissed) {
+        setState(() {
+          isFavorite = false;
+        });
+      }
+    });
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _animationController.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return IconButton(
-      icon: Icon(
-        Icons.favorite,
-        color: Colors.grey[400],
-        size: 30,
-      ),
-      onPressed: () {},
+    return AnimatedBuilder(
+      animation: _animationController,
+      builder: (context, child) {
+        return IconButton(
+          icon: Icon(
+            Icons.favorite,
+            color: _colorAnimation.value,
+            size: 30,
+          ),
+          onPressed: () {
+            isFavorite ? _animationController.reverse() : _animationController.forward();
+          },
+        );
+      },
     );
   }
 }
